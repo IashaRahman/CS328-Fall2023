cad8db42ac9ec8a23d464933bf5182adc9c2dc2b
2101-farias-cyris <90444565+2101-farias-cyris@users.noreply.github.com>
2023-12-09 11:41:20 -0800

    s

---
+++ Assets/Scripts/Boss/Boss1Weapon.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1Weapon : MonoBehaviour
+{
+    public int attackDamage = 20;
+
+    public Vector3 attackOffset;
+    public float attackRange = 1f;
+    public LayerMask attackMask;
+
+    public void Attack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null)
+        {
+            Player player = colInfo.GetComponent<Player>();
+            if (player != null)
+            {
+                player.TakeDamage();
+            }
+            else
+            {
+                Debug.LogWarning("Collider hit, but the object does not have a Player component.");
+            }
+        }
+        else
+        {
+            Debug.Log("No collider hit!");
+        }
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Gizmos.DrawWireSphere(pos, attackRange);
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	private SpriteRenderer[] img;
+	private SpriteRenderer spriteRenderer;
+
+	public Animator animator;
+
+	public int maxHealth = 100;
+	public int currentHealth;
+
+	public HealthBar healthBar;
+	public GameOver gameOver;
+
+	private float movementX;
+	private float movementY;
+	public float speed;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+	}
+
+	void Update()
+	{
+		DoIMove();
+
+		animator.SetFloat("Speed", Mathf.Abs(movementX));
+	}
+
+	void DoIMove()
+	{
+		Vector2 v = new Vector2();
+
+		if (Input.GetKey(KeyCode.W))
+			v.y = 0.1f;
+		if (Input.GetKey(KeyCode.S))
+			v.y = -0.1f;
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			v.x = -0.1f;
+			spriteRenderer.flipX = true;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			v.x = 0.1f;
+			spriteRenderer.flipX = false;
+		}
+
+		movementX = v.x;
+		movementY = v.y;
+	}
+
+	public void TakeDamage()
+	{
+		currentHealth -= 10;
+
+		healthBar.SetHealth(currentHealth);
+
+		animator.SetTrigger("Hurt");
+
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+	}
+
+	void Die()
+	{
+		Debug.Log("You died!");
+
+		animator.SetBool("isDead", true);
+
+		FindObjectOfType<Boss1>().PlayerDied();
+
+		GetComponent<Collider2D>().enabled = false;
+		this.enabled = false;
+
+		StartCoroutine(ShowGameOverScreen());
+	}
+
+	IEnumerator ShowGameOverScreen()
+	{
+		yield return new WaitForSeconds(5f);
+
+		gameOver.GameOverScreen();
+	}
+
+	void FixedUpdate()
+	{
+		Vector2 movement = new Vector2(movementX, movementY);
+
+		rb.MovePosition(rb.position + movement);
+	}
+
+	public void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collided");
+		if (collision.gameObject.tag == "SpeedBoost")
+		{
+			speed *= 4;
+			Destroy(collision.gameObject);
+		}
+		else if (collision.gameObject.tag == "HealthBoost")
+		{
+			currentHealth += 50;
+			Destroy(collision.gameObject);
+		}
+	}
+
+}
