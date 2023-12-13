4bc8418bf0aef97fd2ea1fcb235cf5976518b14c
IashaRahman <iasharahman@nevada.unr.edu>
2023-12-13 09:32:58 -0800

    Completed speed boost power-up

--- Assets/Scripts/PowerUp/SpeedBoost.cs
+++ Assets/Scripts/PowerUp/SpeedBoost.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class speedBoost : MonoBehaviour
 {
+    public float multiplier = 1.5f;
+    public float duration = 8f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            speedPickup(other);
+            StartCoroutine(speedPickup(other));
         }
 
     }
 
-    void speedPickup(Collider2D player)
+    IEnumerator speedPickup(Collider2D player)
     {
         Debug.Log("Speed Power up picked up!");
 
         // Apply effect to player
-        Player playerScript = player.GetComponent<Player>();
+        Player speedBoost = player.GetComponent<Player>();
+        speedBoost.speed *= multiplier;
 
-        // Need speed variable
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+
+        // Wait x amount of seconds
+        yield return new WaitForSeconds(duration);
+
+        // Reverse effect on player
+        speedBoost.speed /= multiplier;
 
         // Remove power up object
         Destroy(gameObject);
