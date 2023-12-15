81de8aa0ad25af73b18b269a1942737b33a0962c
2101-farias-cyris <90444565+2101-farias-cyris@users.noreply.github.com>
2023-12-15 08:00:22 -0800

    level2

---
+++ Assets/Boss2FollowPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 3f;
+void Start(){
+    
+}
+    void Update()
+    {
+        if (player != null)
+        {
+            
+            float distance = Vector2.Distance(transform.position, player.position);
+            transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
